a2b9405b23d79686ac4cf10c6e7c63afbce04113
空影星辉 <wlgc1801lzh@163.com>
2020-05-18 12:33:43 +0800

    update install.sh.

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-pacman -S xorg xorg-server xorg-xinit xf86-video-vesa fish alacritty curl git sudo ttf-dejavu wqy-microhei firefox python python-pip neofetch ctags ranger habak thunderbird fcitx fcitx-configtool fcitx-im fcitx-googlepinyin gimp kdenlive blender audacity jdk11-openjdk libreoffice-fresh
+pacman -S base-devel xorg xorg-server xorg-xinit xf86-video-vesa fish alacritty curl git sudo ttf-dejavu wqy-microhei firefox python python-pip neofetch ctags ranger habak thunderbird fcitx fcitx-configtool fcitx-im fcitx-googlepinyin gimp kdenlive blender audacity jdk11-openjdk libreoffice-fresh
 
 if [ -e "/etc/X11/xinit/xinitrc" ]
 then
