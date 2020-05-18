49cc5122bdcc9dee5e706ee1b924ab8dfd8d8eb9
空影星辉 <wlgc1801lzh@163.com>
2020-05-18 12:16:32 +0800

    update install.sh.

--- install.sh
+++ install.sh
@@ -1,17 +1,10 @@
-pacman -S xorg xorg-server xorg-xinit xf86-video-vesa fish alacritty wget curl git sudo ttf-dejavu wqy-microhei firefox python python-pip neofetch ctags
+pacman -S xorg xorg-server xorg-xinit xf86-video-vesa fish alacritty curl git sudo ttf-dejavu wqy-microhei firefox python python-pip neofetch ctags ranger habak thunderbird fcitx fcitx-configtool fcitx-im fcitx-googlepinyin gimp kdenlive blender audacity jdk11-openjdk nvm libreoffice-fresh
 
 if [ -e "/etc/X11/xinit/xinitrc" ]
 then
     cp /etc/X11/xinit/xinitrc ~/.xinitrc
 fi
 
-wget -P ~ http://gitee.com/wlgc1801lzh/config/raw/master/.alacritty.yml
-wget -P ~ http://gitee.com/wlgc1801lzh/config/raw/master/vim/.vimrc
-
-if [ ! -d "~/dwm" ]
-then
-    mkdir ~/dwm
-    wget -P ~/dwm https://dl.suckless.org/dwm/dwm-6.2.tar.gz
-    tar zxvf ~/dwm/dwm-6.2.tar.gz -C ~/dwm
-    rm ~/dwm/dwm-6.2.tar.gz
-fi
+git clone https://gitee.com/wlgc1801lzh/config.git ~
+cp ~/config/.alacritty.yml ~
+cp ~/config/.vimrc ~
