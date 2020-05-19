0888ae06d1a497c1fdea9aa000f38f91c5343863
wlgc1801lzh <wlgc1801lzh@163.com>
2020-05-19 16:27:39 +0800

    dwm tagbar number

--- dwm-6.2/config.def.h
+++ dwm-6.2/config.def.h
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+static const char *tags[] = { "一", "二", "三", "四", "五", "六", "七", "八", "九" };
 
 static const Rule rules[] = {
 	/* xprop(1):
